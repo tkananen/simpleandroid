61d8a951d1509bba1fd08151ebf4c9ef35871982
Tommy Kananen <tkananen@udel.edu>
2015-04-28 09:37:22 -0400

    final!

--- src/com/example/q1/TraceActivity.java
+++ src/com/example/q1/TraceActivity.java
@@ -11,7 +11,52 @@ public class TraceActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_trace);
+		
 		Log.i("Tracer", "On Create Executed, traceactivity");
 	}
+	
+	@Override
+	protected void onStart(){
+		super.onStart();
+		Log.i("Tracer", "On Start Executed, traceactivity");
+		
+	}
+	
+	@Override 
+	protected void onResume(){
+		super.onResume();
+		Log.i("Tracer", "On Resume Executed, traceactivity");	
+		
+	}
+	
+	@Override
+	protected void onPause(){
+		super.onPause();
+		Log.i("Tracer", "On Pause Executed, traceactivity");
+		
+	}
+	
+	@Override
+	protected void onStop(){
+		super.onStop();
+		Log.i("Tracer", "On Stop Executed, traceactivity");
+	
+	}
+	
+	@Override 
+	protected void onRestart(){
+		super.onRestart();
+		Log.i("Tracer", "On Restart Executed, traceactivity");
+		
+	}
+	
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		Log.i("Tracer", "On Destroy Executed, traceactivity");
+		
+	}
+	
+	
+	
 }
